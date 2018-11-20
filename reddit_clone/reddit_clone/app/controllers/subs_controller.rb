68f0adecd29c77172911370c4bbbdcee8426c447
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-20 12:14:46 -0800

    Specify posts & subs controller

--- reddit_clone/reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/reddit_clone/app/controllers/subs_controller.rb
@@ -1,19 +1,51 @@
 class SubsController < ApplicationController
+  
+  before_action :require_login
+  
   def new
+    @sub = Sub.new
+    render :new
   end
 
   def create
+    @sub = Sub.new(sub_params)
+    @sub.user_id = current_user.id
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash[:errors] = @sub.errors.full_messages
+      render :new
+    end
   end
 
   def edit
+    @sub = Sub.find(params[:id])
+    render :edit
   end
 
   def update
+    @sub = current_user.subs.find(params[:id])
+    # Test for empty sub
+    if @sub.update_attributes(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash[:errors] = @sub.errors.full_messages
+      render :edit
+    end
   end
 
   def index
+    @subs = Sub.all
+    render :index
   end
 
   def show
+    @sub = Sub.find(params[:id])
+    render :show
+  end
+  
+  private
+  def sub_params
+    params.require(:sub).permit(:title, :description)
   end
 end
--- reddit_clone/reddit_clone/config/routes.rb
+++ reddit_clone/reddit_clone/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-  resources :subs, except: [:destroy]
-  resources :posts, except: [:index]
+  resources :subs, except: [:destroy] do
+    resources :posts, only: [:create]
+  end
+  resources :posts, except: [:index, :create]
 end
