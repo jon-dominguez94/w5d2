bc148adf7120e1b185c5464bd80836388894a7ec
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-20 11:09:21 -0800

    Implement User and Sessions controller

--- reddit_clone/reddit_clone/app/controllers/sessions_controller.rb
+++ reddit_clone/reddit_clone/app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      # redirect_to index_url
+    else
+      flash[:errors] = ["Invalid Credentials"]
+      render :new 
+    end
   end
 
   def destroy
+    logout
+    redirect_to new_session_url
   end
 end
